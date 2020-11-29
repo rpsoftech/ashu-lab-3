ef1c920a9e9b11f8cce15ef208d3c3645c4cf2de
Keyur Shah <keyurshah3939@gmail.com>
2020-11-30 00:15:04 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { SocketService } from './socket-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ashuLab3';
+  selected = '';
+  show = false;
+  dirArray: string[] = [];
+  watchDirArray: string[] = [];
+  constructor(public socketservice: SocketService, private zone: NgZone) {
+    socketservice.dirArrayObse.subscribe((a) => {
+      if (a !== null) {
+        zone.run(() => {
+          this.dirArray = a;
+          this.selected = '';
+        });
+        // this.cd.detectChanges();
+      }
+    });
+  }
+  singleClick(d: string) {
+    this.selected = this.selected === d ? '' : d;
+    this.show = this.selected === '' ? false : true;
+    // this.cd.detectChanges();
+  }
+  AddYoSync(a: string): void {
+    this.watchDirArray.push(a);
+    this.socketservice.AddToSync(a);
+  }
+  RemoveFromSync(a: string): void {
+    this.watchDirArray.splice(this.watchDirArray.indexOf(a), 1);
+    this.socketservice.DeSync(a);
+  }
+  close() {
+    this.socketservice.swal
+      .fire({
+        title: 'Are You Sure..??',
+        text: 'You Want To Close this..',
+        icon: 'warning',
+        showCancelButton: true,
+        // confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, Close it!',
+        cancelButtonColor: '#d33',
+      })
+      .then((a) => {
+        if (a.isConfirmed) {
+          this.socketservice.close();
+        }
+      });
+  }
 }
---
+++ src/app/socket-service.service.ts
@@ -0,0 +1,335 @@
+import { Injectable } from '@angular/core';
+import * as net from 'net';
+import * as path from 'path';
+import { BehaviorSubject, pipe, Subject } from 'rxjs';
+import { first } from 'rxjs/operators';
+import * as Swal from 'sweetalert2';
+import { Remote, OpenDialogReturnValue } from 'electron';
+import * as fs1 from 'fs';
+import { environment } from 'src/environments/environment';
+declare var require: any;
+export interface CommunicatinMSG {
+  type: number | string;
+  msg?: string;
+  data?: {
+    [key: string]: any;
+  };
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private remoteElectron: Remote;
+  swal = Swal.default;
+  socket: net.Socket;
+  fs: typeof fs1;
+  conntected = true;
+  path: typeof path;
+  ServerRespoObse: Subject<CommunicatinMSG> = new Subject<CommunicatinMSG>();
+  ServerSendObse: Subject<CommunicatinMSG> = new Subject<CommunicatinMSG>();
+  private host: string;
+  private port: string;
+  patharray: string[] = [];
+  dirArrayObse: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(null);
+  private basicPath: string = '';
+  name = '';
+  private netObj: typeof net;
+  constructor() {
+    this.netObj = window.require('net');
+    this.remoteElectron = window.require('electron').remote;
+    this.path = window.require('path');
+    this.fs = window.require('fs');
+    // require=()=>{}
+    this.socket = new this.netObj.Socket();
+    this.ServerSendObse.subscribe((a) => {
+      this.socket.write(JSON.stringify(a));
+    });
+    this.socket
+      .on('data', (a) => {
+        try {
+          const c = JSON.parse(a.toString());
+          console.log(c);
+          this.ServerRespoObse.next(c);
+        } catch (e) {
+          console.log(e);
+        }
+      })
+      .on('close', () => {
+        this.swal
+          .fire({
+            title: 'Error',
+            text: 'Server Disconnected',
+            icon: 'error',
+            confirmButtonText: 'Restart It',
+            showCancelButton: true,
+            cancelButtonText: 'Close It',
+          })
+          .then((a) => {
+            if (a.isConfirmed) {
+              this.DestroyObj();
+            } else {
+              this.socket.destroy();
+              this.remoteElectron.getCurrentWindow().close();
+            }
+          });
+      });
+    // this.init();
+    this.setObsevableLisnet();
+  }
+  private createDir(p: string): void {
+    console.log(p);
+    this.fs.mkdirSync(p);
+  }
+  private deleteFolderRecursive(path1: string, deletecurrent = true): void {
+    if (this.fs.existsSync(path1)) {
+      this.fs.readdirSync(path1).forEach((file) => {
+        // Get Full Path
+        const curPath = this.path.join(path1, file);
+        // Check If It is Dir
+        if (this.fs.lstatSync(curPath).isDirectory()) {
+          // recurse
+          this.deleteFolderRecursive(curPath);
+        } else {
+          // delete file
+          this.fs.unlinkSync(curPath);
+        }
+      });
+      // Delete Empty Dir
+      if (deletecurrent) {
+        this.fs.rmdirSync(path1);
+      }
+    }
+  }
+  private OpenFolderSelection(): Promise<OpenDialogReturnValue> {
+    // return {} as any;
+    return this.remoteElectron.dialog.showOpenDialog(
+      this.remoteElectron.getCurrentWindow(),
+      {
+        properties: ['openDirectory'],
+        buttonLabel: 'Select',
+        defaultPath: this.remoteElectron.app.getPath('desktop'),
+        title: 'Choose Dir To Sync Server Dir',
+      }
+    );
+  }
+  setObsevableLisnet(): void {
+    this.ServerRespoObse.subscribe(async (c) => {
+      if (c.type === -1) {
+        this.swal
+          .fire({
+            icon: 'error',
+            title: 'Error',
+            text: c.msg,
+            confirmButtonText: 'Yes Restart It.!!',
+            cancelButtonText: 'Close this..',
+            showCancelButton: true,
+          })
+          .then((c1) => {
+            if (c1.isConfirmed) {
+              location.reload();
+            } else {
+              this.close();
+            }
+          });
+      } else if (c.type === -2) {
+        if (c.msg === 'name') {
+          this.swal.fire({
+            icon: 'success',
+            title: 'Welcome To Dir Server ' + this.name + '..!!',
+            timer: 3000,
+          });
+          this.GetAndUpdateArray();
+        } else {
+          this.swal.fire('Error', c.msg, 'error');
+        }
+      } else if (c.type === 1) {
+        this.swal.fire('Success', 'Folder Is Synchronised', 'success');
+      } else if (c.type === 2) {
+        this.swal.fire('Success', 'Folder Is De-Synchronised', 'success');
+      } else if (c.type === 'add') {
+        this.createDir(
+          this.path.join(this.basicPath, c.data.watch, c.data.path)
+        );
+      } else if (c.type === 'delete') {
+        this.deleteFolderRecursive(
+          this.path.join(this.basicPath, c.data.watch, c.data.path)
+        );
+      }
+    });
+  }
+  async init(): Promise<void> {
+    try {
+      if (environment.production) {
+        const a = await this.OpenFolderSelection();
+        if (a.canceled) {
+          throw Error('Please Select Dir Sync Path');
+        }
+        this.basicPath = a.filePaths[0];
+      } else {
+        this.basicPath = '/home/keyur/Desktop/test';
+      }
+      console.log(this.basicPath);
+
+      if (!this.port && !this.host) {
+        const result: any = await this.swal
+          .mixin({
+            input: 'text',
+            confirmButtonText: 'Next &rarr;',
+            showCancelButton: true,
+            progressSteps: ['1', '2'],
+          })
+          .queue([
+            {
+              title: 'Please Enter Host Name.',
+              text: 'Enter The Host Name To Connect',
+            },
+            {
+              title: 'Please Enter Port Number.',
+              text: 'Enter The Port Number To Connect',
+            },
+          ]);
+        if (result.value) {
+          if (isNaN(+result.value[1])) {
+            throw new Error('Please Enter Valid Port Number');
+          }
+          this.host = result.value[0];
+          this.port = result.value[1];
+        }
+      }
+      this.socket.connect(this.port as any, this.host, () => {});
+      const t = setTimeout(() => {
+        this.swal.fire('Error', 'Something went Wrong', 'error').then(() => {
+          this.DestroyObj();
+        });
+      }, 11000);
+      const c = await this.ServerRespoObse.pipe(first()).toPromise();
+      clearTimeout(t);
+      if (c.type !== 0) {
+        throw new Error('Server Responded with Invalid');
+      }
+      if (environment.production) {
+        let timerInterval;
+        const name = await this.swal.fire({
+          icon: 'question',
+          title: c.msg,
+          html: 'Server Will Wait For <b></b> milliseconds.',
+          input: 'text',
+          // inputValidator:console.log,
+          timer: 10000,
+          willOpen: () => {
+            timerInterval = setInterval(() => {
+              const content = this.swal.getContent();
+              if (content) {
+                const b = content.querySelector('b');
+                if (b) {
+                  b.textContent = this.swal.getTimerLeft() as any;
+                }
+              }
+            }, 100);
+          },
+          onClose: () => {
+            clearInterval(timerInterval);
+          },
+        });
+        this.name = name.value;
+        if (name.value) {
+          this.ServerSendObse.next({
+            type: 0,
+            msg: name.value,
+          });
+        }
+      } else {
+        this.name = 'keyurshah';
+        this.ServerSendObse.next({
+          type: 0,
+          msg: 'keyurshah',
+        });
+      }
+      await this.ServerRespoObse.pipe(first()).toPromise();
+      this.GetAndUpdateArray();
+      // this.socket.connect(5000, 'localhost', () => {
+      //   console.log('asdasd');
+
+      //   this.socket.write(
+      //     JSON.stringify({
+      //       type: 0,
+      //       msg: 'keyurshah',
+      //     })
+      //   );
+      // });
+    } catch (e) {
+      this.swal
+        .fire({
+          title: 'Error',
+          icon: 'error',
+          text: e.message,
+          confirmButtonText: 'Yes Restart It.!!',
+          cancelButtonText: 'Close this..',
+          showCancelButton: true,
+        })
+        .then((a) => {
+          if (a.isConfirmed) {
+            this.init();
+          } else {
+            this.swal
+              .fire({
+                title: 'Okay Byyy!',
+                timer: 1500,
+                text: 'See You again!',
+                showConfirmButton: false,
+              })
+              .then(() => {
+                this.close();
+              });
+          }
+        });
+    }
+  }
+  async GetAndUpdateArray(): Promise<void> {
+    this.ServerSendObse.next({
+      type: 5,
+      msg: '',
+    });
+    const l = await this.ServerRespoObse.pipe(first()).toPromise();
+    this.dirArrayObse.next(l.msg as any);
+  }
+  async DeSync(p: string): Promise<void> {
+    this.ServerSendObse.next({
+      type: 2,
+      msg: p,
+    });
+    await this.ServerRespoObse.pipe(first()).toPromise();
+    this.deleteFolderRecursive(this.path.join(this.basicPath, p));
+  }
+  async AddToSync(p: string): Promise<void> {
+    this.createDir(this.path.join(this.basicPath, p));
+    this.ServerSendObse.next({
+      type: 1,
+      msg: p,
+    });
+    await this.ServerRespoObse.pipe(first()).toPromise();
+    this.ServerSendObse.next({
+      type: 5,
+      msg: p,
+    });
+    const a = await this.ServerRespoObse.pipe(first()).toPromise();
+    const dir: string[] = a.msg as any;
+    dir.forEach((c) => {
+      this.createDir(this.path.join(this.basicPath, p, c));
+    });
+  }
+  close(): void {
+    if (Array.isArray(this.dirArrayObse.value)) {
+      this.dirArrayObse.value.forEach((a) => {
+        this.deleteFolderRecursive(this.path.join(this.basicPath, a));
+      });
+    }
+    setTimeout(() => {
+      this.remoteElectron.getCurrentWindow().close();
+    });
+  }
+  private DestroyObj() {
+    this.socket.destroy();
+    location.reload();
+  }
+}
